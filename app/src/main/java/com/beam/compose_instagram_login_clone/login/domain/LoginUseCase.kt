4444eed513d4ab45ed9e163f653c306bc799846f
breduarm <bryan.e.armijos@gmail.com>
2024-04-29 01:49:05 -0500

    Inject all dependencies

--- app/src/main/java/com/beam/compose_instagram_login_clone/login/domain/LoginUseCase.kt
+++ app/src/main/java/com/beam/compose_instagram_login_clone/login/domain/LoginUseCase.kt
@@ -3,8 +3,7 @@ package com.beam.compose_instagram_login_clone.login.domain
 import com.beam.compose_instagram_login_clone.login.data.repository.LoginRepository
 import javax.inject.Inject
 
-class LoginUseCase @Inject constructor() {
-    private val repository = LoginRepository()
+class LoginUseCase @Inject constructor(private val repository: LoginRepository) {
 
     suspend operator fun invoke(user: String, password: String): Boolean =
         repository.login(user, password)
